f0e767f2530d08b3232dfb4504630cff3007b4de
S.B. <silvio.bernardinello@gmail.com>
2014-08-22 12:15:06 +0200

    Started Genetic Algorithm

---
+++ src/algo/communitydetection/GeneticAlgorithm.java
@@ -0,0 +1,22 @@
+package algo.communitydetection;
+
+import graph.IGraph;
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+
+public class GeneticAlgorithm {
+	protected final IGraph g;
+
+	public GeneticAlgorithm(IGraph g) {
+		super();
+		this.g = g;
+	}
+	
+	public void execute() {
+		
+	}
+	
+	public Int2IntMap getResult() {
+		return null;
+	}
+	
+}
