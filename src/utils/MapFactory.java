99e73ef923e7b7c64e6ee1df0e7fdbd7f809d761
S.B. <silvio.bernardinello@gmail.com>
2014-08-23 14:46:14 +0200

    Testing AVLTrees

--- src/utils/MapFactory.java
+++ src/utils/MapFactory.java
@@ -1,28 +1,32 @@
 package utils;
 
+import it.unimi.dsi.fastutil.ints.Int2FloatAVLTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2FloatMap;
-import it.unimi.dsi.fastutil.ints.Int2FloatOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2IntAVLTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
-import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2LongAVLTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2LongMap;
-import it.unimi.dsi.fastutil.ints.Int2LongOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectAVLTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
 public class MapFactory {
 	public static Int2FloatMap int2FloatMap() {
-		return new Int2FloatOpenHashMap();
+//		return new Int2FloatOpenHashMap();
+		return new Int2FloatAVLTreeMap();
 	}
 	
 	public static Int2IntMap int2IntMap() {
-		return new Int2IntOpenHashMap();
+//		return new Int2IntOpenHashMap();
+		return new Int2IntAVLTreeMap();
 	}
 	
 	public static Int2ObjectMap<Int2IntMap> int2ObjectMap() {
-		return new Int2ObjectOpenHashMap<>();
+//		return new Int2ObjectOpenHashMap<>();
+		return new Int2ObjectAVLTreeMap<>();
 	}
 	
 	public static Int2LongMap int2LongMap() {
-		return new Int2LongOpenHashMap();
+//		return new Int2LongOpenHashMap();
+		return new Int2LongAVLTreeMap();
 	}
 }
